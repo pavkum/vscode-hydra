1286d3e4783ed49dcbbe14e8d4bb38790f68892e
Pavan Kumar L <kumarl@adobe.com>
2019-03-29 00:30:55 +0530

    removed build and test files

--- test/utils.test.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from 'vscode';
-
-export async function updateConfiguration(key: string, value: any) {
-    const settings = vscode.workspace.getConfiguration('hydra');
-    await settings.update(key, value);
-}
